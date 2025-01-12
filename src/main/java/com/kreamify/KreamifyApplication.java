450cfb784d713fd8866b4529bd1b0742a4224dff
bum0w0 <kgy5988@naver.com>
2025-01-12 21:48:52 +0900

    [feat] 프로젝트 초기 세팅

---
+++ src/main/java/com/kreamify/KreamifyApplication.java
@@ -0,0 +1,13 @@
+package com.kreamify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreamifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreamifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kreamify/KreamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
